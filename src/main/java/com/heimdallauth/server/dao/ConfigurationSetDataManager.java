6034254246e46da8340a2b085467ed4e8f8a38ee
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-03-22 02:46:11 -0600

    HAS-36 improve(DAO): Implement DataManager for Configuration Sets

--- src/main/java/com/heimdallauth/server/dao/ConfigurationSetDataManager.java
+++ src/main/java/com/heimdallauth/server/dao/ConfigurationSetDataManager.java
@@ -1,9 +1,11 @@
 package com.heimdallauth.server.dao;
 
 import com.heimdallauth.server.constants.SmtpEncryption;
+import com.heimdallauth.server.models.ConfigurationSetModel;
 import com.heimdallauth.server.utils.HeimdallMetadata;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface ConfigurationSetDataManager {
     boolean isConfigurationSetNameExist(String configurationSetName);
@@ -12,4 +14,6 @@ public interface ConfigurationSetDataManager {
     void updateConfigurationSetSuppressionList(String configurationSetId, String suppressionListEntry, String suppressionListEntryType);
     void updateConfigurationSetSmtpProperties(String configurationSetId, String host, String port, String username, String password, SmtpEncryption encryption);
     void deleteConfigurationSet(String configurationSetId);
+    ConfigurationSetModel getConfigurationSetById(UUID configurationSetId);
+    ConfigurationSetModel getConfigurationSetByName(String configurationSetName, UUID tenantId);
 }
---
+++ src/main/java/com/heimdallauth/server/services/ConfigurationSetManagementService.java
@@ -0,0 +1,53 @@
+package com.heimdallauth.server.services;
+
+import com.heimdallauth.server.dao.ConfigurationSetDataManager;
+import com.heimdallauth.server.exceptions.ConfigurationSetAlreadyExists;
+import com.heimdallauth.server.exceptions.HeimdallBifrostBadDataException;
+import com.heimdallauth.server.models.ConfigurationSetModel;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+import static com.heimdallauth.server.constants.HeimdallBifrostExceptionMessages.CONFIGURATION_SET_EXISTS;
+
+@Service
+@Slf4j
+public class ConfigurationSetManagementService {
+    private final ConfigurationSetDataManager configurationSetDataManager;
+
+    @Autowired
+    public ConfigurationSetManagementService(ConfigurationSetDataManager configurationSetDataManager) {
+        this.configurationSetDataManager = configurationSetDataManager;
+    }
+
+    public ConfigurationSetModel getConfigurationSetModel(String configurationSetId){
+        try{
+            UUID configurationSetUUID = UUID.fromString(configurationSetId);
+            return configurationSetDataManager.getConfigurationSetById(configurationSetUUID);
+        }catch (IllegalArgumentException e){
+            log.error("Invalid configuration set id: {}", configurationSetId);
+            return null;
+        }
+    }
+    public ConfigurationSetModel createConfigurationSet(String tenantId, String configurationSetName, String configurationSetDescription) throws HeimdallBifrostBadDataException {
+        try{
+            validateConfigurationSetName(configurationSetName);
+            this.configurationSetDataManager.saveConfigurationSet(configurationSetName, configurationSetDescription);
+            return this.configurationSetDataManager.getConfigurationSetByName(configurationSetName, UUID.fromString(tenantId) );
+        }catch (ConfigurationSetAlreadyExists e){
+            log.error("Configuration set with the same name already exists: {}", configurationSetName);
+            throw new HeimdallBifrostBadDataException(e.getMessage());
+        }catch (IllegalArgumentException e){
+            log.error("Invalid tenant id: {}", tenantId);
+            throw new HeimdallBifrostBadDataException("Invalid tenant id");
+        }
+    }
+
+    private void validateConfigurationSetName(String configurationSetName){
+        if(this.configurationSetDataManager.isConfigurationSetNameExist(configurationSetName)){
+            throw new ConfigurationSetAlreadyExists(CONFIGURATION_SET_EXISTS);
+        }
+    }
+}
