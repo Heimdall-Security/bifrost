5dbab652a8b45d7852e5be3d9484d8bd87e8bb40
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-01 16:09:20 -0700

    HAS-39 fix(API): Updates to fix to create configuration set with proper tenant ID

--- src/main/java/com/heimdallauth/server/controllers/v1/management/ConfigurationSetManagementController.java
+++ src/main/java/com/heimdallauth/server/controllers/v1/management/ConfigurationSetManagementController.java
@@ -26,7 +26,7 @@ public class ConfigurationSetManagementController {
     }
     @PostMapping("/create")
     public ResponseEntity<ConfigurationSetModel> createNewConfigurationSet(@RequestBody CreateConfigurationSetDTO createConfigurationSetDTO, @RequestParam("force") boolean force){
-        ConfigurationSetModel createdConfigurationSet = this.configurationSetManagementService.createNewConfigurationSet(createConfigurationSetDTO, UUID.randomUUID(), force);
+        ConfigurationSetModel createdConfigurationSet = this.configurationSetManagementService.createNewConfigurationSet(createConfigurationSetDTO, createConfigurationSetDTO.tenantId(), force);
         return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdConfigurationSet.configurationSetId()).toUri()).build();
     }
     @GetMapping
