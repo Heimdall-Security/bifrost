0eb780c43e0d6a7f0a49c73a7179a1e6c32de055
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-03-31 17:31:29 -0700

    HAS-39 improve(manage): Update to the EmailSuppression Management Service Interface

--- src/main/java/com/heimdallauth/server/services/EmailSuppressionManagementService.java
+++ src/main/java/com/heimdallauth/server/services/EmailSuppressionManagementService.java
@@ -5,11 +5,14 @@ import com.heimdallauth.server.exceptions.SuppressionListNotFound;
 import com.heimdallauth.server.models.bifrost.SuppressionEntryModel;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface EmailSuppressionManagementService {
+    List<SuppressionEntryModel> getAllSuppressionEntries();
     List<SuppressionEntryModel> getAllSuppressionEntriesById(List<UUID> suppressionEntryId) throws SuppressionListNotFound;
     SuppressionEntryModel createSuppressionEntry(CreateSuppressionEntryDTO createSuppressionEntryPayload);
-    SuppressionEntryModel getSuppressionEntryByConfigurationSetId(String configurationSetId) throws SuppressionListNotFound;
-    void deleteSuppressionEntryById(String suppressionEntryId);
+    SuppressionEntryModel getSuppressionEntryById(UUID suppressionEntryId) throws SuppressionListNotFound;
+    List<SuppressionEntryModel> getSuppressionEntryByConfigurationSetId(UUID configurationSetId) throws SuppressionListNotFound;
+    void deleteSuppressionEntryById(UUID suppressionEntryId);
 }
