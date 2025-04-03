32c6686583eac031b6a6dbff4c5ad18a8c424bd8
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-03 00:39:29 -0700

    HAS-39 improve(security): Updates to create custom security role mappings for endpoints

--- src/main/java/com/heimdallauth/server/BifrostApplication.java
+++ src/main/java/com/heimdallauth/server/BifrostApplication.java
@@ -1,9 +1,14 @@
 package com.heimdallauth.server;
 
+import com.heimdallauth.server.configuration.HeimdallBifrostRoleConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        HeimdallBifrostRoleConfiguration.class
+})
 public class BifrostApplication {
 
     public static void main(String[] args) {
