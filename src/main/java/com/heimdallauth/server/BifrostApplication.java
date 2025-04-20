add88acacd75267d141a697a59f486ed1ddeaf86
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-20 02:42:39 -0700

    HAS-61 improve(GlobalSuppressionClient): Update to implement FeignClient for Global Suppressions

--- src/main/java/com/heimdallauth/server/BifrostApplication.java
+++ src/main/java/com/heimdallauth/server/BifrostApplication.java
@@ -5,12 +5,14 @@ import com.heimdallauth.server.configuration.HeimdallOauth2ClientConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
         HeimdallBifrostRoleConfiguration.class,
         HeimdallOauth2ClientConfiguration.class
 })
+@EnableFeignClients
 public class BifrostApplication {
 
     public static void main(String[] args) {
